e5b47b5ddd222921ee5aca0aedceee44cfc341c7
iawn <ian.oldenburg@gmail.com>
2017-01-06 14:39:45 -0800

    added batch functionality to Cluster3DMC so all depths processed concurrently

--- Cluster3DMC.m
+++ Cluster3DMC.m
@@ -1,4 +1,4 @@
-function Cluster3DMC(file,loadStart,numToLoad,rect,nDepths);
+function Cluster3DMC(directory,loadStart,numToLoad,rect,basename,parallelDepths)
 
 % This code will motion correct and do pixel CI for all of the image stacks
 % located in the specified directory
@@ -6,58 +6,79 @@ function Cluster3DMC(file,loadStart,numToLoad,rect,nDepths);
 %file = '/global/scratch/mardinly/AM63_1a/20160303_trimOff/';
 %loadStart=1;
 %numToLoad=304;
+%basename is a variable term if omitted will select all files in directory
 
-if ~exist('rect');
-rect=[1 1 511 511];
-rect=repmat(rect,[nDepths 1]);
-disp('no rectangle, using whole image')
-elseif numel(rect(:,1))~=nDepths;
-rect=[1 1 511 511];
-rect=repmat(rect,[nDepths 1]);
-disp('rect did not have 4 elements, using whole image')
-end;
-
-for n=1:nDepths
-wiTemp=rect(n,1):rect(n,1)+rect(n,3);
-hiTemp=rect(n,2):rect(n,2)+rect(n,4);
-
-%check Range Errors
-wiTemp(wiTemp>=511)=[];
-wiTemp(wiTemp<=1)=[];
-hiTemp(hiTemp>=511)=[];
-hiTemp(hiTemp<=1)=[];
-
-wi{n}=wiTemp;
-hi{n}=hiTemp;
-
-end
 
 
 
 %if dir is selected, make sure you provide a direcotry
-if exist(file)~=7;
+if exist(directory)~=7;
     errordlg('Please provide directory')
 end;
 
-
+if nargin<5 || isempty(basename) || ~ischar(basename)
+    basename ='';
+end
 
 
 %% get all files in dir
-k=dir(file);k(1:2)=[];
+fprintf('Identifying Files... \n'); 
+k=dir(directory);k(1:2)=[];
 i=1;
 for n=1:(numel(k))
     fn=k(n).name;
-    if strcmp(fn(length(fn)-2:length(fn)),'tif')
-        filenames{i}=[file  k(n).name];
+    if regexp(fn,regexptranslate('wildcard',[basename '*.tif']))
+        filenames{i}=fullfile(directory,  k(n).name);
         i=i+1;
     end;
 end;
+fprintf([ num2str(numel(filenames)) ' Files Detected... \n']);
 
 %% Extract MetaData
 
 s = imfinfo(filenames{1});
 header = s(1).Software;
 MD = parseSI5Header(header); 
+
+%% set rectangle
+try
+    nDepths = MD.hFastZ.numFramesPerVolume;
+catch
+    nDepths = 1;
+end
+
+
+if ~exist('rect');
+    rect=[1 1 511 511];
+    rect=repmat(rect,[nDepths 1]);
+    disp('no rectangle, using whole image')
+elseif ~all(size(rect) == [nDepths 4])
+    rect=[1 1 511 511];
+    rect=repmat(rect,[nDepths 1]);
+    disp('rect did not have 4 elements, using whole image')
+elseif size(rect,1) ~= nDepths
+    rect=repmat(rect(1,:),[nDepths 1]);
+    disp('rect only included for one depth, appying to all')
+end;
+
+
+
+for n=1:nDepths
+    wiTemp=rect(n,1):rect(n,1)+rect(n,3);
+    hiTemp=rect(n,2):rect(n,2)+rect(n,4);
+    
+    %check Range Errors
+    wiTemp(wiTemp>=511)=[];
+    wiTemp(wiTemp<=1)=[];
+    hiTemp(hiTemp>=511)=[];
+    hiTemp(hiTemp<=1)=[];
+    
+    wi{n}=wiTemp;
+    hi{n}=hiTemp;
+    
+end
+
+
 
 
 %% check and see if .align file exists already in dir
@@ -70,8 +91,47 @@ k=f{1}; k(length(k)-6:length(k))=[];
 outputBase=[k num2str(loadStart) '-' num2str(numToLoad+loadStart-1)];
 useTheseFiles=filenames(nn)';
 
-for depth=1:nDepths;
+% parallelDepths =0;
 
+if parallelDepths
+    
+    
+    nc = feature('numcores'); %number of cores
+    availCores = nc -nDepths; %each depth takes a core to launch
+    minCore = floor(availCores/nDepths);
+    jobCores = ones(nDepths,1)*minCore;
+    remCore = rem(availCores,nDepths);
+    for i=1:remCore %distribute unused cores across jobs
+        jobCores(i)=jobCores(i)+1;
+    end
+    
+    
+    for depth=1:nDepths;
+        
+        j(depth) = batch(['sbxAlignOneDepth(outputBase,useTheseFiles,hi,wi,MD,' num2str(depth) ')'],...
+            'Pool',jobCores(depth));
+        
+    end;
+    
+    %wait for jobs to end
+    for i=numel(j)
+        disp(['Waiting for job ' num2str(i) ' (others might still be running)']);
+        wait(j(i));
+        disp(['Diary for Depth ' num2str(i) ': ']);
+        diary(j(i));
+    end
+    disp('jobs completed');
+else
+    for depth=1:nDepths;
+        sbxAlignOneDepth(outputBase,useTheseFiles,hi,wi,MD,depth)
+    end
+    disp('job completed');
+end
+
+
+end
+
+function sbxAlignOneDepth(outputBase,useTheseFiles,hi,wi,MD,depth)
 OPB=[outputBase '_depth_' num2str(depth)];
 disp('Beginning MC')
 sbxalignmastermulti_3D(useTheseFiles,1,[OPB '.align'],hi{depth},wi{depth},MD,depth);
@@ -84,7 +144,6 @@ save([OPB '.align'],'hi','wi','-append');
 try
 save([OPB '.align'],'rect','-append');
 end
-
 %sbxcomputeci(useTheseFiles,[outputBase '.align'],hi{depth},wi{depth}); %Takes about 10 minutes, eats up a ton of RAM
 
-end;
+end
